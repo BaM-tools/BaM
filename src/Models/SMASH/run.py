a7114285553751b6b21731b143f24850e8f8f226
benRenard <benjamin.renard@inrae.fr>
2022-08-03 17:39:35 +0200

    Interfacing SMASH model continuing. Not fully functionnal yet, but all the pieces are there.

--- src/Models/SMASH/run.py
+++ src/Models/SMASH/run.py
@@ -5,22 +5,25 @@ import numpy
 import rasterio
 import matplotlib.pyplot as plt
 
-
+# Interpret command-line arguments
 projectDir=sys.argv[1]
+thetaDir=sys.argv[2]
+# go into project directory
 os.chdir(projectDir)
-
 # load model from pickle (avoids re-reading inputs etc.)
 model=smash.read_pickle("model.pkl")
-
-# Define parameter
-# uniform 
-# model.parameters.cp=180. 
-# distributed
-myCP = rasterio.open("myCP.asc").read(1).transpose()
-model.parameters.cp=myCP.copy()
-
+# Define distributed parameters
+cp = rasterio.open(os.path.join(thetaDir,"cp.asc")).read(1).transpose()
+model.parameters.cp=cp.copy()
+ctr = rasterio.open(os.path.join(thetaDir,"ctr.asc")).read(1).transpose()
+model.parameters.ctr=ctr.copy()
+cr = rasterio.open(os.path.join(thetaDir,"cr.asc")).read(1).transpose()
+model.parameters.cr=cr.copy()
+ml = rasterio.open(os.path.join(thetaDir,"ml.asc")).read(1).transpose()
+model.parameters.ml=ml.copy()
 # Run
 model2=model.direct_run()
+# Plot
 model2.plot_hydrograph()
 plt.show()
 
--- tests/BaM_SMASH/testSMASH_main.f90
+++ tests/BaM_SMASH/testSMASH_main.f90
@@ -3,16 +3,64 @@ program testSMASH
 use kinds_dmsl_kit
 use SMASH_model
 use utilities_dmsl_kit,only:getSpareUnit
+use DataRW_tools,only:ReadRasterASC, WriteRasterASC
+use Geodesy_tools,only:rasterGridType,rasterToPoints,interpolateOnGrid
 
 implicit none
 
 character(len_vLongStr), parameter:: setupFile='../../tests/BaM_SMASH/Config_setup.txt'
-logical, parameter:: reload=.false.
-
-integer(mik):: err,unt
+logical, parameter:: reload=.true.
+integer(mik):: err,unt,i
 character(250):: mess,startDate,endDate
-character(len_vLongStr)::loadScript,runScript,projectDir,precipDir,petDir
+character(len_vLongStr)::loadScript,runScript,projectDir,precipDir,petDir,thetaDir
 character(len_uLongStr)::cmdString
+type(rasterGridType)::grid
+real(mrk),allocatable::M(:,:)
+real(mrk),allocatable::points(:,:)
+real(mrk)::anchor(1,3),anchors(4,3)
+
+! Get grid
+call ReadRasterASC(file='/home/brenard/Desktop/2022SMASH/V5004030/PARAM/FLOW.asc',gridOnly=.true.,grid=grid,M=M,err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+
+! Paramter cp
+anchors(1,:)=(/grid%xllcorner,grid%yllcorner,500._mrk/)
+anchors(2,:)=(/grid%xllcorner+(grid%ncols+1)*grid%cellsize,grid%yllcorner,800._mrk/)
+anchors(3,:)=(/grid%xllcorner,grid%yllcorner+(grid%nrows+1)*grid%cellsize,1000._mrk/)
+anchors(4,:)=(/grid%xllcorner+(grid%ncols+1)*grid%cellsize,grid%yllcorner+(grid%nrows+1)*grid%cellsize,100._mrk/)
+call interpolateOnGrid(pts=anchors,grid=grid,method='IDW',par=(/1._mrk/),M=M,err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+call WriteRasterASC(grid=grid,M=M,file='/home/brenard/Desktop/2022SMASH/V5004030/THETA/cp.asc',err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+
+! Paramter ctr
+anchor(1,:)=(/grid%xllcorner,grid%yllcorner,100._mrk/)
+call interpolateOnGrid(pts=anchor,grid=grid,method='IDW',par=(/1._mrk/),M=M,err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+call WriteRasterASC(grid=grid,M=M,file='/home/brenard/Desktop/2022SMASH/V5004030/THETA/ctr.asc',err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+
+! Paramter cr
+anchor(1,:)=(/grid%xllcorner,grid%yllcorner,600._mrk/)
+call interpolateOnGrid(pts=anchor,grid=grid,method='IDW',par=(/1._mrk/),M=M,err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+call WriteRasterASC(grid=grid,M=M,file='/home/brenard/Desktop/2022SMASH/V5004030/THETA/cr.asc',err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+
+! Paramter cr
+anchor(1,:)=(/grid%xllcorner,grid%yllcorner,0._mrk/)
+call interpolateOnGrid(pts=anchor,grid=grid,method='IDW',par=(/1._mrk/),M=M,err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+call WriteRasterASC(grid=grid,M=M,file='/home/brenard/Desktop/2022SMASH/V5004030/THETA/ml.asc',err=err,mess=mess)
+if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+
+! test rasterToPoints sub
+!call rasterToPoints(grid=grid,M=M,pts=points,err=err,mess=mess)
+!if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
+!do i=1,44 !size(points,dim=1)
+!    write(*,*) points(i,:)
+!enddo
+!read(*,*)
 
 call getSpareUnit(unt,err,mess)
 if(err/=0) then;write(*,*) trim(mess);read(*,*);endif
@@ -28,6 +76,8 @@ read(unt,*,iostat=err) precipDir
 if(err>0) then;write(*,*) trim(mess);read(*,*);endif
 read(unt,*,iostat=err) petDir
 if(err>0) then;write(*,*) trim(mess);read(*,*);endif
+read(unt,*,iostat=err) thetaDir
+if(err>0) then;write(*,*) trim(mess);read(*,*);endif
 read(unt,*,iostat=err) startDate
 if(err>0) then;write(*,*) trim(mess);read(*,*);endif
 read(unt,*,iostat=err) endDate
@@ -40,7 +90,7 @@ if(reload) then
     call execute_command_line (trim(cmdString),exitStat=err,cmdMsg=mess)
 endif
 
-cmdString='python3'//' '//runScript//' '//projectDir
+cmdString='python3'//' '//runScript//' '//projectDir//' '//thetaDir
 call execute_command_line (trim(cmdString),exitStat=err,cmdMsg=mess)
 
 write(*,*) 'Exit status:', err

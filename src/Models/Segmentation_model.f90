e4c161643bb905767ea3fe83bc0e22e812c12c68
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-02-01 18:01:40 +0100

    Modify the content of segmentation model from segmentation2 model

--- src/Models/Segmentation_model.f90
+++ src/Models/Segmentation_model.f90
@@ -4,7 +4,8 @@ module Segmentation_model
 !~**********************************************************************
 !^* Programmer: Ben Renard & Matteo Darienzo, Irstea Lyon
 !^**********************************************************************
-!^* Last modified: 23/10/2018
+!^* Last modified: 23/10/2019 - allow using inter-shift durations
+!^*                rather than shift times + both tmin and nmin constraints
 !~**********************************************************************
 
 
@@ -14,7 +15,6 @@ module Segmentation_model
 !~* 2Do List:
 !~**********************************************************************
 !~* Quick description of public procedures:
-
 !~**********************************************************************
 
 use kinds_dmsl_kit ! numeric kind definitions from DMSL
@@ -53,12 +53,8 @@ err=0;mess=''
 npar= nS + nS-1
 end subroutine Segmentation_GetParNumber
 
-
-
-
-
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine Segmentation_Apply(time,nS,tmin,theta,Y,feas,err,mess)
+subroutine Segmentation_Apply(time,nS,tmin,nmin,theta,option,Y,feas,err,mess)
 !^**********************************************************************
 !^* Purpose: apply the
 !^**********************************************************************
@@ -73,91 +69,88 @@ subroutine Segmentation_Apply(time,nS,tmin,theta,Y,feas,err,mess)
 !^* 2Do List:
 !^**********************************************************************
 !^* IN
-!^*    1. nS, number of sources
-!
-!
+!^*    1. time
+!^*    2. nS, number of segments
+!^*    3. tmin, minimum segment duration
+!^*    4. nmin, minimum number of points per segment
+!^*    5. theta, parameter vector
+!^*    6. option. 1: shift times, 2: inter-shift durations
 !^* OUT
 !^*    1. Y, output vector
-!^*    2.
-!^*    3.feas, feasibility (all theta between 0 and 1)
-!^*    4.err, error code; <0:Warning, ==0:OK, >0: Error
-!^*    5.mess, error message
+!^*    2.feas, feasibility (all theta between 0 and 1)
+!^*    3.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*    4.mess, error message
 !^**********************************************************************
-integer(mik), intent(in)::nS
+integer(mik), intent(in)::nS,nmin,option
 real(mrk), intent(in):: tmin, time(:), theta(:)
 real(mrk), intent(out)::Y(:)
 logical, intent(out)::feas
 integer(mik), intent(out)::err
 character(*),intent(out)::mess
 !locals
+character(250),parameter::procname='Segmentation_Apply'
 integer(mik)::i,nt,indx
-real(mrk)::tau(nS-1),mu(nS), taumin, taumax
+real(mrk)::duration(nS-1),tau(nS-1),mu(nS)
+integer(mik)::segment(size(time))
 
 err=0;
 mess='';
 Y=undefRN
 feas = .TRUE.
-!if(a(1)<=0._mrk .or. c(1)==0._mrk) then
-!    feas=.false.;return
-!endif
+nt=size(time)
 
+!size check
 if (size(theta) /= (2*nS -1)) then
     feas =.FALSE.
     return
 end if
 
+! Single-segment case is trivial, treat it and get out
+if(nS==1) then;Y=theta(1);return;endif
+
+! From now on, multi-segment case
+! Get means
 mu =theta(1:nS)
-
-if (nS > 1) then
-   tau =theta((nS+1):)
-endif
-
-nt=size(time)
-
-if (nS >= 3) then
-  do i= 1, (nS-2)
-    if (tau(i+1) <= tau(i)) then
-        feas = .FALSE.
-        return
-    end if
-    if ((tau(i+1) - tau(i))< tmin) then
-        feas = .FALSE.
-        return
-    end if
-    enddo
-endif
-
-
-
-if (nS > 1) then
-    taumin = minval(time)
-    taumax = maxval(time)
-   if (any(tau <= (taumin+tmin)) .or. any(tau >= (taumax-tmin))) then
-    feas = .FALSE.
-    return
-   end if
-endif
-
-
-
-
-do i=1,nt
-    if (nS==1) then
-        Y(i) = mu(1)
-    else
-        indx=GettRange(time(i),tau)
-        Y(i) = mu(indx)
+! Get inter-shift durations and shift times
+select case (option)
+case(1) ! parameters are shift times
+    tau =theta((nS+1):)
+    duration(1)=tau(1)
+    if(nS>2) then
+        do i=2,nS-1
+            duration(i)=tau(i)-tau(i+1)
+        enddo
     endif
+case(2) ! parameters are inter-shift durations
+    duration =theta((nS+1):)
+    do i=1,nS-1
+        tau(i)=sum(duration(1:i))
+    enddo
+case default
+    err=1;mess=trim(procname)//': Fatal: Unavailable [option]';return
+end select
+
+! Feasability check
+if(any(duration<=0.)) then;feas=.false.;return;endif
+if(any(duration<=tmin)) then;feas=.false.;return;endif
+if(any(tau>=maxval(time))) then;feas=.false.;return;endif
+
+! Determine segment of each point
+do i=1,nt
+    segment(i)=GettRange(time(i),tau)
 enddo
 
+! Check that there are at least nmin values for each segment
+do i=1,nS
+    if(count(segment==i)<nmin) then;feas=.false.;return;endif
+enddo
 
-
+! Get mean of each segment
+do i=1,nt
+    Y(i) = mu(segment(i))
+enddo
 
 end subroutine Segmentation_Apply
-
-
-
-
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 subroutine Segmentation_XtraRead(file,xtra,err,mess)
@@ -199,11 +192,13 @@ read(unt,*,iostat=err) xtra%is1 ! nS  number of segments
 if(err/=0) then;mess=trim(procname)//':problem reading file '//trim(file);return;endif
 read(unt,*,iostat=err) xtra%rs1 ! tmin between two segments
 if(err/=0) then;mess=trim(procname)//':problem reading file '//trim(file);return;endif
+read(unt,*,iostat=err) xtra%is2 ! nmin minimum number of values per segment
+if(err/=0) then;mess=trim(procname)//':problem reading file '//trim(file);return;endif
+read(unt,*,iostat=err) xtra%is3 ! option
+if(err/=0) then;mess=trim(procname)//':problem reading file '//trim(file);return;endif
 close(unt)
 end subroutine Segmentation_XtraRead
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 
 function GettRange(t,tau)
 !#**********************************************************************

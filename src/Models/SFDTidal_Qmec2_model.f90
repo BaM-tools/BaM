af1c3a78b5475086689b83155d038009f0ffe72f
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2024-07-09 09:17:18 +0200

    The Qmec2 model will use water level in absolute elevation during calculation, instead of elevation relative to chart datum as in the original Qmec model.
    This assumption will modify the conversion of water elevation(h) from chart datum (d) to absolute elevation (y).
    
    Structural change:
    
    Old model : y1 = h1 - d1 ; y2 = h2 - d2 ; (pre-processing in Qmec, we misunderstood the conversion due to reference point)
    New model : y1 = h1 + d1 ; y2 = h2 + d2 ;
    
    The new model is more logical, because the stage record from chart datum is taken into account as input data, but both stations should have the same reference point (e.g. sea level) to correctly the hydraulic slope.
    
    Reparametrisation :
    1) dzeta_qmec = d2 - d1 + delta
    2) dh_qmec + dzeta_qmec = dy + delta
    3) hm_qmec = ym - (d1+d2)/2

--- src/Models/SFDTidal_Qmec2_model.f90
+++ src/Models/SFDTidal_Qmec2_model.f90
@@ -35,7 +35,7 @@ pure subroutine SFDTidal_Qmec2_GetParNumber(npar,err,mess)
 !^**********************************************************************
 !^* Programmer: Felipe Mendez-Rios, Ben Renard, INRAE
 !^**********************************************************************
-!^* Last modified: 05/07/2024
+!^* Last modified: 08/07/2024
 !^**********************************************************************
 !^* Comments:
 !^**********************************************************************
@@ -54,20 +54,23 @@ integer(mik), intent(out)::npar,err
 character(*),intent(out)::mess
 ! locals
 
-err=0;mess='';npar=13 !Be, bevs, b1, b2, delta, ne, d1, d2, c, g, Q0, dx, dt
+err=0;mess='';npar=11 !Be, bevs,delta, ne, d1, d2, c, g, Q0, dx, dt
 
 end subroutine SFDTidal_Qmec2_GetParNumber
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine SFDTidal_Qmec2_Apply(h1,h2,theta,Q,pressure_gradient,gravity_force,bottom_friction,advection,feas,err,mess)
+subroutine SFDTidal_Qmec2_Apply(h1,h2,theta,Q,pressure_gradient,bottom_friction,advection,feas,err,mess)
 
 !^**********************************************************************
 !^* Purpose: apply SFDTidal Qmec2 model
 !^**********************************************************************
 !^* Programmer: Felipe Mendez-Rios, Ben Renard, INRAE
 !^**********************************************************************
-!^* Last modified: 05/07/2024
+!^* Last modified: 08/07/2024
 !^**********************************************************************
 !^* Comments:
+!^* Qmec original is calibrated using stages (h) from chart datum (d).
+!^* Qmec BaM is calibrated using stages from sea level (y or Zw).
+!^* The relation both variable is y = h + d
 !^**********************************************************************
 !^* References: 10.1029/2019JC015992
 !^**********************************************************************
@@ -84,19 +87,19 @@ subroutine SFDTidal_Qmec2_Apply(h1,h2,theta,Q,pressure_gradient,gravity_force,bo
 !^*     4. mess, error message
 !^**********************************************************************
 real(mrk), intent(in)::h1(:),h2(:),theta(:)
-real(mrk), intent(out)::Q(:),pressure_gradient(:), gravity_force(:), bottom_friction(:), advection(:)
+real(mrk), intent(out)::Q(:),pressure_gradient(:), bottom_friction(:), advection(:)
 logical, intent(out)::feas(:)
 integer(mik), intent(out)::err
 character(*),intent(out)::mess
 !locals
 character(250),parameter::procname='SFDTidal_Qmec2_Apply'
-real(mrk)::Be, bevs, b1, b2, delta, ne, d1, d2, c, g,Q0, dx, dt, pg, bf, ad, gv
-real(mrk)::dhdt1, dhdt2
-real(mrk)::y1(size(h1)),y2(size(h2)),dh(size(h1)),hm(size(h1)),Ae(size(h1)),Pe(size(h1)),Rhe(size(h1))
+real(mrk)::Be, bevs, delta, ne, d1, d2, c, g,Q0, dx, dt, pg, bf, ad
+real(mrk)::dydt1, dydt2
+real(mrk)::y1(size(h1)),y2(size(h2)),dy(size(h1)),ym(size(h1)),Ae(size(h1)),Pe(size(h1)),Rhe(size(h1))
 integer(mik)::nm, m
 
 err=0;mess='';feas=.true.;Q=undefRN
-pressure_gradient=undefRN;bottom_friction=undefRN;advection=undefRN;gravity_force=undefRN
+pressure_gradient=undefRN;bottom_friction=undefRN;advection=undefRN
 nm=size(h1)
 
 ! Check feasability
@@ -106,35 +109,33 @@ endif
 
 ! Make sense of inputs and theta
 Be=theta(1)    !effective width B_e [m]
-bevs=theta(2)  !effective riverbed elevation [m] (he_qmec = -bevs)
-b1=theta(3)    !riverbed in upstream gauge station] (dzeta_qmec = b1 - b2 + delta)
-b2=theta(4)    !riverbed in downstream gauge station] (dzeta_qmec = b1 - b2 + delta)
-delta=theta(5) !riverbed error leveling [m] (dzeta_qmec = b1 - b2 + delta)
-ne=theta(6)    !effective Manning's coefficient [s m^(-1/3)]
-d1=theta(7)    !upstream chart datum [m]
-d2=theta(8)    !downstream chart datum [m]
-c=theta(9)     !exponent[]
-g=theta(10)    !gravity[m/s2]
-Q0=theta(12)   !initial discharge [m3/s]
-dx=theta(12)   !distance between stations
-dt=theta(13)   !time step
+bevs=theta(2)  !effective riverbed elevation from sea level [m] (he_qmec = -bevs)
+delta=theta(3) !riverbed error leveling [m] (dzeta_qmec = d2 - d1 + delta)
+ne=theta(4)    !effective Manning's coefficient [s m^(-1/3)]
+d1=theta(5)    !upstream chart datum [m]
+d2=theta(6)    !downstream chart datum [m]
+c=theta(7)     !exponent[]
+g=theta(8)     !gravity[m/s2]
+Q0=theta(9)    !initial discharge [m3/s]
+dx=theta(10)   !distance between stations
+dt=theta(11)   !time step
 
 ! Check feasability
 if(Be<=0._mrk .or. ne<=0._mrk .or. c<=0._mrk .or. g<=0._mrk .or. dx<=0._mrk .or. dt<=0._mrk) then
     feas=.false.;return
 endif
 
-! water levels minus datums
-y1 = h1-d1
-y2 = h2-d2
+! Conversion of water level from chart datum level to absolute elevation (Zw)
+y1 = h1+d1
+y2 = h2+d2
 
 ! Intermediate calculations
-dh = y2 - y1           ! Difference between the two stations
-hm = (y1 + y2)/2._mrk  ! Mean between the two stations
+dy = y2 - y1           ! Difference between the two stations   (dh_qmec + dzeta_qmec = dy + delta) (see pressure gradient calculation)
+ym = (y1 + y2)/2._mrk  ! Mean between the two stations (hm_qmec = ym - (d1+d2)/2)
 
 ! Geometry parameters for a rectangular cross-section
-Ae = Be*(hm - bevs)          ! Mean cross sectional area
-Pe = Be + 2._mrk*(hm - bevs) ! Wetted perimeter
+Ae = Be*(ym - bevs)          ! Mean cross sectional area
+Pe = Be + 2._mrk*(ym - bevs) ! Wetted perimeter
 Rhe = Ae/Pe                  ! Hydraulic radius
 
 ! Check feasability
@@ -147,48 +148,41 @@ Q(1)=Q0
 pressure_gradient(1)=0
 bottom_friction(1)=0
 advection(1)=0
-gravity_force(1)=0
 
 do m=2,nm
     if (m==2) then
         !First-order scheme for the first time-step with Q(1) = Q0
-        dhdt1 = (hm(m) - hm(m-1))/(dt)
+        dydt1 = (ym(m) - ym(m-1))/(dt)
 
-        pg = -g*(Ae(m-1)*(dh(m-1))/dx)
-
-        gv = -g*(Ae(m-1)*(b1-b2+delta)/dx)
+        pg = -g*(Ae(m-1)*(dy(m-1)+delta)/dx) ! (dh_qmec + dzeta_qmec = dy + delta)
 
         bf = -g*((ne**2)/(Rhe(m-1)**(c)))*Q(m-1)*abs(Q(m-1))/Ae(m-1)
 
-        ad = 2._mrk*(Q(m-1)/Ae(m-1))*Be*dhdt1
+        ad = 2._mrk*(Q(m-1)/Ae(m-1))*Be*dydt1
 
     else
-        dhdt1 = (hm(m) - hm(m-2))/(2*dt)
+        dydt1 = (ym(m) - ym(m-2))/(2*dt)
         if (m == 3) then
-            dhdt2 = (hm(m-1) - hm(m-2))/dt
+            dydt2 = (ym(m-1) - ym(m-2))/dt
         else
-            dhdt2 = (hm(m-1) - hm(m-3))/(2*dt)
+            dydt2 = (ym(m-1) - ym(m-3))/(2*dt)
         endif
         !Second-order Adams-Bashforth scheme
 
-        pg = (1.5_mrk)*(-g*Ae(m-1)*(dh(m-1))/dx) &
-                           -(0.5_mrk)*(-g*Ae(m-2)*(dh(m-2))/dx)
-
-        gv = (1.5_mrk)*(-g*Ae(m-1)*(b1-b2+delta)/dx) &
-                           -(0.5_mrk)*(-g*Ae(m-2)*(b1-b2+delta)/dx)
+        pg = (1.5_mrk)*(-g*Ae(m-1)*(dy(m-1)+delta)/dx) &
+                           -(0.5_mrk)*(-g*Ae(m-2)*(dy(m-2)+delta)/dx)
 
         bf = (1.5_mrk)*(-g*((ne**2)/(Rhe(m-1)**(c)))*Q(m-1)*abs(Q(m-1))/Ae(m-1))&
                            -(0.5_mrk)*(-g*((ne**2)/(Rhe(m-2)**(c)))*Q(m-2)*abs(Q(m-2))/Ae(m-2))
 
-        ad = (1.5_mrk)*(2._mrk*(Q(m-1)/Ae(m-1))*Be*dhdt1) &
-                           -(0.5_mrk)*(2._mrk*(Q(m-2)/Ae(m-2))*Be*dhdt2)
+        ad = (1.5_mrk)*(2._mrk*(Q(m-1)/Ae(m-1))*Be*dydt1) &
+                           -(0.5_mrk)*(2._mrk*(Q(m-2)/Ae(m-2))*Be*dydt2)
     endif
 
-    Q(m) = Q(m-1) + dt*(pg+gv+bf+ad)
+    Q(m) = Q(m-1) + dt*(pg+bf+ad)
     pressure_gradient(m) = dt*pg
     bottom_friction(m) = dt*bf
     advection(m) = dt*ad
-    gravity_force(m) = dt*gv
 
 enddo
 
